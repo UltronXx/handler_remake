cf95d05de1b2002f98177773f3f539e6b614fdd3
UltronXx <91951535+UltronXx@users.noreply.github.com>
2021-11-29 14:02:34 +0000

    Add files via upload
    Software resources

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace HMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel4 = new Guna.UI.WinForms.GunaLabel();
+            this.processReaderLabel = new Guna.UI.WinForms.GunaLabel();
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.progressBar = new Guna.UI.WinForms.GunaProgressBar();
+            this.startButton = new Guna.UI2.WinForms.Guna2Button();
+            this.quitButton = new Guna.UI2.WinForms.Guna2Button();
+            this.aboutButton = new Guna.UI2.WinForms.Guna2Button();
+            this.helpButton = new Guna.UI2.WinForms.Guna2Button();
+            this.developerButton = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Metropolis Extra Bold", 52F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel1.Location = new System.Drawing.Point(106, 153);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(419, 140);
+            this.gunaLabel1.TabIndex = 0;
+            this.gunaLabel1.Text = "Get started\r\nhere";
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Metropolis", 13F);
+            this.gunaLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.gunaLabel2.Location = new System.Drawing.Point(115, 329);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(462, 36);
+            this.gunaLabel2.TabIndex = 0;
+            this.gunaLabel2.Text = "Organise and keep your devices. Start using Handler\r\nnow for easy devices record " +
+    "keeping.";
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Metropolis", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.gunaLabel3.Location = new System.Drawing.Point(815, 671);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(86, 20);
+            this.gunaLabel3.TabIndex = 0;
+            this.gunaLabel3.Text = "Handler";
+            // 
+            // gunaLabel4
+            // 
+            this.gunaLabel4.AutoSize = true;
+            this.gunaLabel4.Font = new System.Drawing.Font("Metropolis", 10F);
+            this.gunaLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.gunaLabel4.Location = new System.Drawing.Point(911, 674);
+            this.gunaLabel4.Name = "gunaLabel4";
+            this.gunaLabel4.Size = new System.Drawing.Size(136, 14);
+            this.gunaLabel4.TabIndex = 0;
+            this.gunaLabel4.Text = "Beta Version 20.2.1";
+            // 
+            // processReaderLabel
+            // 
+            this.processReaderLabel.AutoSize = true;
+            this.processReaderLabel.Font = new System.Drawing.Font("Metropolis", 10F);
+            this.processReaderLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.processReaderLabel.Location = new System.Drawing.Point(115, 442);
+            this.processReaderLabel.Name = "processReaderLabel";
+            this.processReaderLabel.Size = new System.Drawing.Size(146, 14);
+            this.processReaderLabel.TabIndex = 0;
+            this.processReaderLabel.Text = "Processes reader .....";
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.Image")));
+            this.gunaPictureBox1.Location = new System.Drawing.Point(694, 99);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Size = new System.Drawing.Size(458, 448);
+            this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.gunaPictureBox1.TabIndex = 1;
+            this.gunaPictureBox1.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.BorderColor = System.Drawing.Color.Black;
+            this.progressBar.ColorStyle = Guna.UI.WinForms.ColorStyle.Default;
+            this.progressBar.IdleColor = System.Drawing.Color.Gainsboro;
+            this.progressBar.Location = new System.Drawing.Point(118, 467);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressMaxColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(186)))), ((int)(((byte)(133)))));
+            this.progressBar.ProgressMinColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(186)))), ((int)(((byte)(133)))));
+            this.progressBar.Size = new System.Drawing.Size(410, 4);
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Value = 65;
+            // 
+            // startButton
+            // 
+            this.startButton.BorderRadius = 12;
+            this.startButton.CheckedState.Parent = this.startButton;
+            this.startButton.CustomImages.Parent = this.startButton;
+            this.startButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(58)))), ((int)(((byte)(119)))));
+            this.startButton.Font = new System.Drawing.Font("Metropolis", 11F);
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.HoverState.Parent = this.startButton;
+            this.startButton.Location = new System.Drawing.Point(118, 514);
+            this.startButton.Name = "startButton";
+            this.startButton.ShadowDecoration.Parent = this.startButton;
+            this.startButton.Size = new System.Drawing.Size(140, 42);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.BorderRadius = 12;
+            this.quitButton.CheckedState.Parent = this.quitButton;
+            this.quitButton.CustomImages.Parent = this.quitButton;
+            this.quitButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(92)))), ((int)(((byte)(246)))));
+            this.quitButton.Font = new System.Drawing.Font("Metropolis", 11F);
+            this.quitButton.ForeColor = System.Drawing.Color.White;
+            this.quitButton.HoverState.Parent = this.quitButton;
+            this.quitButton.Location = new System.Drawing.Point(288, 514);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.ShadowDecoration.Parent = this.quitButton;
+            this.quitButton.Size = new System.Drawing.Size(140, 42);
+            this.quitButton.TabIndex = 3;
+            this.quitButton.Text = "Quit";
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // aboutButton
+            // 
+            this.aboutButton.BorderRadius = 12;
+            this.aboutButton.CheckedState.Parent = this.aboutButton;
+            this.aboutButton.CustomImages.Parent = this.aboutButton;
+            this.aboutButton.FillColor = System.Drawing.Color.White;
+            this.aboutButton.Font = new System.Drawing.Font("Metropolis", 11F);
+            this.aboutButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.aboutButton.HoverState.Parent = this.aboutButton;
+            this.aboutButton.Location = new System.Drawing.Point(118, 668);
+            this.aboutButton.Name = "aboutButton";
+            this.aboutButton.ShadowDecoration.Parent = this.aboutButton;
+            this.aboutButton.Size = new System.Drawing.Size(115, 40);
+            this.aboutButton.TabIndex = 4;
+            this.aboutButton.Text = "About";
+            // 
+            // helpButton
+            // 
+            this.helpButton.BorderRadius = 12;
+            this.helpButton.CheckedState.Parent = this.helpButton;
+            this.helpButton.CustomImages.Parent = this.helpButton;
+            this.helpButton.FillColor = System.Drawing.Color.White;
+            this.helpButton.Font = new System.Drawing.Font("Metropolis", 11F);
+            this.helpButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.helpButton.HoverState.Parent = this.helpButton;
+            this.helpButton.Location = new System.Drawing.Point(243, 668);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.ShadowDecoration.Parent = this.helpButton;
+            this.helpButton.Size = new System.Drawing.Size(115, 40);
+            this.helpButton.TabIndex = 4;
+            this.helpButton.Text = "Help";
+            // 
+            // developerButton
+            // 
+            this.developerButton.BorderRadius = 12;
+            this.developerButton.CheckedState.Parent = this.developerButton;
+            this.developerButton.CustomImages.Parent = this.developerButton;
+            this.developerButton.FillColor = System.Drawing.Color.White;
+            this.developerButton.Font = new System.Drawing.Font("Metropolis", 11F);
+            this.developerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.developerButton.HoverState.Parent = this.developerButton;
+            this.developerButton.Location = new System.Drawing.Point(368, 668);
+            this.developerButton.Name = "developerButton";
+            this.developerButton.ShadowDecoration.Parent = this.developerButton;
+            this.developerButton.Size = new System.Drawing.Size(115, 40);
+            this.developerButton.TabIndex = 4;
+            this.developerButton.Text = "Developer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1247, 761);
+            this.Controls.Add(this.developerButton);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.aboutButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.gunaPictureBox1);
+            this.Controls.Add(this.gunaLabel4);
+            this.Controls.Add(this.gunaLabel3);
+            this.Controls.Add(this.processReaderLabel);
+            this.Controls.Add(this.gunaLabel2);
+            this.Controls.Add(this.gunaLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel4;
+        private Guna.UI.WinForms.GunaLabel processReaderLabel;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
+        private Guna.UI.WinForms.GunaProgressBar progressBar;
+        private Guna.UI2.WinForms.Guna2Button startButton;
+        private Guna.UI2.WinForms.Guna2Button quitButton;
+        private Guna.UI2.WinForms.Guna2Button aboutButton;
+        private Guna.UI2.WinForms.Guna2Button helpButton;
+        private Guna.UI2.WinForms.Guna2Button developerButton;
+    }
+}
+
